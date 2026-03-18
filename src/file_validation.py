0bc6f97829078f0e7b52d21a99f079b47b1bd38b
reymonera <castillo.vilcahuaman@gmail.com>
2026-03-18 11:50:11 +0100

    normalization done

--- src/file_validation.py
+++ src/file_validation.py
@@ -4,41 +4,39 @@ import argparse
 from .utils import GlobalTimer
 
 # This function should be the one putting the taxa in a
-# browsable list for the tree prune function to work.
+# browsable list for the tree prune function to work. This
+# also contains a normalization process, in which the sum of
+# sample columns is evaluated. Every sample column should sum 1.
 def get_validation_otu_table(filepath):
     try:
         with open(filepath, 'r') as f:
             first_line = f.readline()
-        
-        if '\t' in first_line:
-            separator = '\t'
-            formato = "TSV"
-        elif ',' in first_line:
-            separator = ','
-            formato = "CSV"
-        else:
-            raise ValueError("Cannot detect separator (expected tab or comma)")
-        
-        df = pd.read_csv(filepath, sep=separator)
-        otu_col = None
-        
-        #otu_col_candidates = ['#OTU ID', 'OTU ID', 'OTU_ID', '#OTU_ID', 'OTUID', 'otu_id']
-        #for candidate in otu_col_candidates:
-        #    if candidate in df.columns:
-        #        otu_col = candidate
-        #        break
-        
-        #if otu_col is None:
-        #    raise ValueError(f"OTU table missing OTU ID column. Expected one of: {otu_col_candidates}")
+            if '\t' in first_line:
+                separator = '\t'
+            elif ',' in first_line:
+                separator = ','
+            else:
+                raise ValueError("Cannot detect separator (expected tab or comma)")
 
-        sample_cols = [col for col in df.columns if col != otu_col]
+        df = pd.read_csv(filepath, sep=separator, index_col=0)
+
+        sample_cols = df.columns.tolist()
+
         if len(sample_cols) <= 1:
             raise ValueError("OTU table has no sample columns")
-        
-        df = pd.read_csv(filepath, sep=separator)
+
+        # Normalization process
+        GlobalTimer.log("Checking if OTU table is normalized...")
+        col_sums = df[sample_cols].sum(axis=0)
+        print(col_sums)
+        if not (abs(col_sums - 1.0) < 1e-6).all():
+            GlobalTimer.log("WARNING: OTU table columns do not sum to 1, normalizing...")
+            df[sample_cols] = df[sample_cols].div(col_sums, axis=1)
+            df.to_csv(filepath, sep=separator, index=True)
+            GlobalTimer.log("✓ OTU table normalized and saved")
+
         GlobalTimer.log("✓ OTU table validated")
-        #print("✓ OTU table validated")
-    
+
     except Exception as e:
         GlobalTimer.log(f"✕ Error validation OTU Table: {e}")
         sys.exit(1)
